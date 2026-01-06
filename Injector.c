8fa935f8520a8ab09c231a3d7cfcca1c8e7c6fe0
compiledkernel-idk <berkkapla82@gmail.com>
2026-01-06 03:35:21 +0000

    Update Injector.c

--- Injector.c
+++ Injector.c
@@ -7,8 +7,10 @@
 #include <sys/wait.h>
 #include <sys/ptrace.h>
 #include <sys/user.h>
+#include <sys/uio.h>
 #include <dlfcn.h>
 #include <fcntl.h>
+#include <errno.h>
 
 pid_t find_pid(const char* name) {
     DIR* d = opendir("/proc");
@@ -31,18 +33,6 @@ pid_t find_pid(const char* name) {
     }
     closedir(d);
     return -1;
-}
-
-int is_ptraced(pid_t pid) {
-    char path[64], line[256];
-    snprintf(path, sizeof(path), "/proc/%d/status", pid);
-    int fd = open(path, O_RDONLY);
-    if (fd < 0) return 1; 
-    read(fd, line, sizeof(line));
-    close(fd);
-    char *tracer = strstr(line, "TracerPid:");
-    if (tracer && atoi(tracer + 10) != 0) return 1;
-    return 0;
 }
 
 unsigned long get_module_base(pid_t pid, const char *name) {
@@ -70,20 +60,20 @@ unsigned long get_offset(const char *lib, const char *sym) {
 }
 
 void inject(pid_t pid, const char *lib_path) {
-    if (is_ptraced(pid)) exit(EXIT_FAILURE);
-
     struct user_regs_struct old, regs;
     unsigned long target_dlopen;
     int status;
 
-    if (ptrace(PTRACE_ATTACH, pid, NULL, NULL) < 0) exit(EXIT_FAILURE);
+    if (ptrace(PTRACE_ATTACH, pid, NULL, NULL) < 0) {
+        perror("ERR_ATTACH");
+        exit(EXIT_FAILURE);
+    }
     waitpid(pid, &status, 0);
     ptrace(PTRACE_GETREGS, pid, NULL, &old);
     memcpy(&regs, &old, sizeof(regs));
 
     unsigned long libc = get_module_base(pid, "libc.so.6");
     unsigned long libdl = get_module_base(pid, "libdl.so.2");
-    
     target_dlopen = libdl ? (libdl + get_offset("libdl.so.2", "dlopen")) : 
                             (libc + get_offset("libc.so.6", "dlopen"));
 
@@ -104,24 +94,43 @@ void inject(pid_t pid, const char *lib_path) {
     regs.rsi = RTLD_NOW;
     regs.rip = target_dlopen;
     regs.rsp -= 8;
-    
     ptrace(PTRACE_POKEDATA, pid, regs.rsp, 0);
-
     ptrace(PTRACE_SETREGS, pid, NULL, &regs);
     ptrace(PTRACE_CONT, pid, NULL, NULL);
     waitpid(pid, &status, 0);
-
     ptrace(PTRACE_SETREGS, pid, NULL, &old);
     ptrace(PTRACE_DETACH, pid, NULL, NULL);
 }
 
+void patch(pid_t pid) {
+    unsigned char pattern[] = "Java_com_roblox_protocols_localstor";
+    size_t p_len = sizeof(pattern) - 1;
+    unsigned long start = 0x700000000000UL, end = 0x800000000000UL, c_size = 0x100000;
+    unsigned char *buf = malloc(c_size);
+    if (!buf) return;
+
+    for (unsigned long addr = start; addr < end; addr += c_size) {
+        struct iovec local = {buf, c_size}, remote = {(void*)addr, c_size};
+        if (process_vm_readv(pid, &local, 1, &remote, 1, 0) > 0) {
+            for (size_t i = 0; i <= c_size - p_len; i++) {
+                if (memcmp(buf + i, pattern, p_len) == 0) {
+                    unsigned long base = (addr + i) - 0x1000d;
+                    unsigned char patch[] = {0x90, 0x90};
+                    struct iovec pl = {patch, 2}, pr = {(void*)(base + 0x0), 2};
+                    process_vm_writev(pid, &pl, 1, &pr, 1, 0);
+                    free(buf);
+                    return;
+                }
+            }
+        }
+    }
+    free(buf);
+}
+
 int main(int argc, char **argv) {
-    const char *target_proc = "sober";
-    const char *lib = (argc > 1) ? argv[1] : "./atingle.so";
-
-    pid_t pid = find_pid(target_proc);
-    if (pid == -1) exit(EXIT_FAILURE);
-
-    inject(pid, lib);
+    pid_t pid = find_pid("sober");
+    if (pid == -1) return EXIT_FAILURE;
+    inject(pid, (argc > 1) ? argv[1] : "./atingle.so");
+    patch(pid);
     return EXIT_SUCCESS;
 }
