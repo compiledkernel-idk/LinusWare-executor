b15a9c63e71b0aeee6b8d403bc1ee45d682824b4
compiledkernel-idk <berkkapla82@gmail.com>
2026-01-10 21:12:37 +0100

    Remove build directory from repo

--- build/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_sirracha-qt_FILE /home/sultan/sirracha-executor/build/sirracha-qt)
-set(__QT_DEPLOY_TARGET_sirracha-qt_TYPE EXECUTABLE)
--- build/CMakeFiles/sirracha-qt.dir/sirracha-qt_autogen/mocs_compilation.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/sirracha-qt.dir/sirracha-qt_autogen/mocs_compilation.cpp.o: \
- /home/sultan/sirracha-executor/build/sirracha-qt_autogen/mocs_compilation.cpp \
- /usr/include/stdc-predef.h
